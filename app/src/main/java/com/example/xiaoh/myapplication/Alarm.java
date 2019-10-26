1fa56cb9bdb86537393ef2f24d865adb65e7d4f3
Xiao <xiao.han1@hotmail.com>
2019-10-26 15:54:24 -0500

    basic alarm outline

--- app/src/main/java/com/example/xiaoh/myapplication/Alarm.java
+++ app/src/main/java/com/example/xiaoh/myapplication/Alarm.java
@@ -1,9 +1,38 @@
 package com.example.xiaoh.myapplication;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Date;
+
 public class Alarm {
     public String name;
+    public Timer t;
+    public Date alarmDate;
+    public boolean isTemp;
+    public boolean isRepeat;
+    public boolean isOn;
 
     Alarm(String name) {
         this.name = name;
+        isOn = false;
+        isRepeat = false;
+        isTemp = false;
+        alarmDate = new Date();
+    }
+
+    Alarm(String name, Date alarmDate) {
+        this(name,alarmDate,false);
+    }
+
+    Alarm(String name, Date alarmDate, boolean isTemp) {
+        this.name = name;
+        this.alarmDate = alarmDate;
+        isTemp = isTemp;
+        isOn = true;
+        isRepeat = false;
+    }
+
+    public long getTime() {
+        return alarmDate.getTime();
     }
 }
