d7e9ce0cc9501fb2aba2bc992c5ac56f3ab0167b
CandyMandy28 <amanda.wang294@gmail.com>
2019-10-26 12:03:29 -0500

    new changes

--- alarm.py
+++ alarm.py
@@ -9,10 +9,16 @@ class Alarm:
         self.time = alarm_time
         self.isTemp = isTemp
         self.isRepeat = isRepeat
+        self.isOn = True
+        # TODO: Snooze
     
-    # isTime - whether it is the time to ring the alarm
-    # returns: whether or not it is time to ring the alarm
-    def isTime():
+    """ 
+    isTime - whether it is the time to ring the alarm
+    param: custom_time = the amount of time before the alarm runs
+    returns: whether or not it is time to ring the alarm
+    """
+    def runAlarm(custom_time = None):
+        
         return True
     
     
--- setup.py
+++ setup.py
@@ -5,7 +5,14 @@ try:
     import string
 except ImportError:
     print("Warning: `string` must be installed in order to use `Code-Ada-2019`")
+
 try:
     import time
 except ImportError:
     print("Warning: `time` must be installed in order to use `Code-Ada-2019`")
+    
+try:
+    import threading
+except ImportError:
+    print("Warning: `threading` must be installed in order to use `Code-Ada-2019`")
+
