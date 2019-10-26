f93bccb937e4a92334e5b5d83c18d2960e10d942
Xiao <xiao.han1@hotmail.com>
2019-10-26 15:25:46 -0500

    Create Android Studio App

---
+++ app/src/main/java/com/example/xiaoh/myapplication/Alarm.java
@@ -0,0 +1,9 @@
+package com.example.xiaoh.myapplication;
+
+public class Alarm {
+    public String name;
+
+    Alarm(String name) {
+        this.name = name;
+    }
+}
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import alarm # get alarm.py
+import alarmManager
 import setup # get setup.py
 
-custom_time = time
+custom_time = setup.time
+
+print("HI")
+
+#while alarmManager has alarms that are on
+
 
